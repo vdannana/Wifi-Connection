7d0fde98e1a6933fa5a6fe81278efd9409d14acf
Dannana Venkataramana <vdannana@netgear.com>
2020-05-26 18:18:24 +0530

    Update Configuration.py

--- Configuration.py
+++ Configuration.py
@@ -1,7 +1,12 @@
+'Wi-Fi SSID'
 SSID = "Venkat_Mobile"
+'Wi-Fi Password'
 password = "Venkat1999"
+'Execution Type'
 option = 2
+'Number of time try to connect to Wi-Fi
 connection_time = 3
+'Profile type is type of Profile for open value is 0 for secured profile value is 1'
 Profile_type = 0
 Open_Profile_File = r'Requirement\Wi-Fi-Open.xml'
 Secured_Profile_File = r'Requirement\Wi-Fi-Secure.xml'
